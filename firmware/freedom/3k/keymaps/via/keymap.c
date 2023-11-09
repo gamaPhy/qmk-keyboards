38dcf24e7fdee180fcf09364625f9d40204db713
tlietz <theodore.lietz@gmail.com>
2023-11-08 21:22:28 -0600

    adds rgb toggle to keymap

--- firmware/freedom/3k/keymaps/via/keymap.c
+++ firmware/freedom/3k/keymaps/via/keymap.c
@@ -10,6 +10,6 @@ enum layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_OSU] = LAYOUT(
         KC_Z,   KC_X,         KC_C,
-        KC_ESC, KC_CALIBRATE, KC_SPACE
+        KC_ESC, KC_CALIBRATE, RGB_TOG
     )
 };
