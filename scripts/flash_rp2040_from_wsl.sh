e0c50104afa8cc9f94d9b6d5fc651da9ce2178cb
Teddy <theodore.lietz@gmail.com>
2024-08-07 23:21:16 -0500

    adds drive letter cli arguemnt to flash rp2040 script

--- scripts/flash_rp2040_from_wsl.sh
+++ scripts/flash_rp2040_from_wsl.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+# Check if the drive letter argument is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <drive_letter>"
+  exit 1
+fi
+
+# Convert the drive letter to uppercase if it isn't already
+drive_letter_upper=$(echo "$1" | tr '[:lower:]' '[:upper:]')
+
+# Convert the drive letter to lowercase if it isn't already
+drive_letter_lower=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+
 # set directory to script file's locations
 cd "$(dirname "$0")"
 cd ../
 ./in_qmk.sh qmk compile -kb gamaphy/freedom/3k -km via -e TEST=1
-sudo mount -t drvfs F: /mnt/f
-cp qmk_firmware/gamaphy_freedom_3k_via.uf2 /mnt/f/
+sudo mount -t drvfs "$drive_letter_upper": /mnt/"$drive_letter_lower"
+cp qmk_firmware/gamaphy_freedom_3k_via.uf2 /mnt/"$drive_letter_lower"/
